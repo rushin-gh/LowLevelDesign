d6121bac7b88a0c284d8fa3bd66b09b9fab74523
Rushi <rushin.mailme@gmail.com>
2025-03-23 21:03:41 +0530

    Demonstrated LSP through examples

---
+++ SOLID/LSP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    namespace Bad
+    {
+        public class Policy
+        {
+            public virtual void MedicalExamination()
+            {
+                Console.WriteLine("Medical examination required.");
+            }
+        }
+
+        public class AutoInsurancePolicy : Policy
+        {
+            public override void MedicalExamination()
+            {
+                throw new NotImplementedException("Auto insurance does not require medical examination!");
+            }
+        }
+
+    }
+
+    namespace Good
+    {
+        public abstract class Policy
+        {
+            public abstract void CalculatePremium();
+        }
+
+        public interface IMedicalExaminationRequired
+        {
+            void MedicalExamination();
+        }
+
+        public class LifeInsurancePolicy : Policy, IMedicalExaminationRequired
+        {
+            public override void CalculatePremium() { /* Premium logic */ }
+            public void MedicalExamination() { /* Examination logic */ }
+        }
+
+        public class AutoInsurancePolicy : Policy
+        {
+            public override void CalculatePremium() { /* Premium logic */ }
+        }
+    }
+}
