693d68346656e306c6ccb4ff37e25807865d9233
Rushi <rushin.mailme@gmail.com>
2025-03-23 20:56:00 +0530

    Demonstrated OCP throgh example

---
+++ SOLID/OCP.cs
@@ -0,0 +1,35 @@
+﻿// Software should be open for extension but closed for modification
+
+namespace OCD
+{
+    namespace Bad
+    {
+        public class PolicyService
+        {
+            public double CalculatePremium(string policyType)
+            {
+                if (policyType == "Life") return 5000;
+                else if (policyType == "Auto") return 2000;
+                else return 1000; // New policyType? Modify this method 😖
+            }
+        }
+    }
+
+    namespace Good
+    {
+        public abstract class Policy
+        {
+            public abstract double CalculatePremium();
+        }
+
+        public class LifeInsurancePolicy : Policy
+        {
+            public override double CalculatePremium() => 5000;
+        }
+
+        public class AutoInsurancePolicy : Policy
+        {
+            public override double CalculatePremium() => 2000;
+        }
+    }
+}
