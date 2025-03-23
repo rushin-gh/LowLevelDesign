391b0561ec99ff0c3cf17f017d51e029f5313d86
Rushi <rushin.mailme@gmail.com>
2025-03-23 20:53:52 +0530

    Demonstrated SRP using examples

---
+++ SOLID/SRP.cs
@@ -0,0 +1,32 @@
+﻿// SRP : A class should have only one reason to change
+
+namespace SRP
+{
+    namespace Bad
+    {
+        public class PolicyService
+        {
+            public void CalculatePremium() { /* Calculates premium */ }
+            public void SavePolicyToDatabase() { /* Saves policy */ }
+            public void SendNotification() { /* Sends notification */ }
+        }
+    }
+
+    namespace Good
+    {
+        public class Policy
+        {
+            public void CalculatePremium() { /* Calculates premium */ }
+        }
+
+        public class PolicyRepository
+        {
+            public void SavePolicyToDatabase() { /* Saves policy */ }
+        }
+
+        public class NotificationService
+        {
+            public void SendNotification() { /* Sends notification */ }
+        }
+    }
+}
